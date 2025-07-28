3048fec81a66f00ecae7fc4dbe6e1362d82b7ffc
Augusto Tintino DEV <augustotintinooficial@gmail.com>
2025-07-28 03:36:20 -0500

    Criação do index.html/ style.css/ app.js e assets para a imagem.

---
+++ dias_da_semana/app.js
@@ -0,0 +1,11 @@
+diasdaSemana = prompt ('Digite o dia:');
+if (diasdaSemana == 'Sabado') {
+alert ('Bom fim de semana');
+}
+else if (diasdaSemana == 'Domingo') {
+  alert ('Bom fim de semana');
+}
+else
+  {
+    alert('Boa semana!');
+  }
