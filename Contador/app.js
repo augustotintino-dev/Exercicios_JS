06edb3a65ba25a5c9d1a843d89c766d7d7430796
Augusto Tintino DEV <augustotintinooficial@gmail.com>
2025-07-29 17:47:40 -0500

    inicial contador

---
+++ Contador/app.js
@@ -0,0 +1,30 @@
+//Contagem progressiva
+let numeroMaximo = prompt('Digite um numero para contagem regressiva: ');
+let contador = 0;
+while(contador <= numeroMaximo) {
+  alert(contador);
+  contador++;
+}
+
+//Contagem regressiva
+// let numeroMaximo = prompt('Digite um numero para contagem regressiva: ');
+// while(numeroMaximo >= 0) {
+//   alert(numeroMaximo);
+//   numeroMaximo--;
+// }
+
+//Contador regressivo
+// let contador = 10;
+// while ( contador >= 0) {
+//   alert(contador);
+//   // contador = contador + 1;
+//   contador--;
+// }
+
+//Contador progressivo
+// let contador = 1;
+// while ( contador <= 0) {
+//   alert(contador);
+//   // contador = contador + 1;
+//   contador++;
+// }
