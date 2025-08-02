b21ca146505df0ad4353c5f5ee75fd9821a0de17
Augusto Tintino DEV <augustotintinooficial@gmail.com>
2025-08-01 20:23:20 -0500

    Vários scripts

---
+++ varios-codigos/app.js
@@ -0,0 +1,67 @@
+// Resolução 01
+// let bemvindo = ('Boas vindas ao código JS');
+// console.log(bemvindo);
+
+//Resolução 02
+// let nome = ('João Augusto Tintino Vasconcelos');
+// console.log(`Olá, ${nome} seja muito bem vindo ao mundo do JAVASCRIPT`);
+
+//REsolução 03
+// let nome = ('João Augusto Tintino Vasconcelos');
+// alert(`Olá, ${nome} seja muito bem vindo ao mundo do JAVASCRIPT`);
+
+//Resolução 04
+
+// let linguagemPreferida = prompt('Qual sua linguagem de programação predileta');
+// console.log(`Sua linguagem de programação preferida é ${linguagemPreferida}`);
+
+//Resolução 05
+// let valor01 = (100);
+// let valor02 = (200);
+// let resultado = valor01 + valor02;
+// console.log(resultado);
+
+//Resolução 06
+// let valor01 = (100);
+// let valor02 = (200);
+// let resultado = valor01 - valor02;
+// console.log(resultado);
+
+//Resolução 07
+// let idade = prompt('Digite a sua idade');
+// if (idade >= 18) {
+//   console.log(`Você é maior de idade e tem ${idade} anos`);
+// }else{
+//   console.log('Você não é maior de idade');
+// }
+
+//Resolusão 08
+// let numero = prompt('Digite um número');
+// if (numero > 1){
+//   console.log('Número positivo');
+// }else {
+//   console.log('Número negativo');
+// }
+
+//Resolução 09
+
+// let numeros = 1;
+// while (numeros <= 10 ){
+//   console.log(numeros);
+//   numeros++;
+// }
+
+//Resolução 10
+
+// let media = 8;
+// let nota = prompt('Digite a nota do aluno');
+// if ( nota >= media) {
+//   alert(`Aluno(a) aprovado com nota ${nota}`);
+// }else {
+//   alert(`Aluno(a) em recuperação com nota ${nota}`);
+// }
+
+//Resolução 11
+
+// let numero = parseInt(Math.random() * 1000 + 1);
+// console.log(`O número aleatório gerado foi ${numero}`);
